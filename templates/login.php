c53ed622fa68cc5206b1de522d27c3ae13062ef2
Anonta <SH-anonta@users.noreply.github.com>
2018-03-21 09:46:51 +0600

    Move client side data vaidation code from template files to seperate js files. For login.php and register.php

--- templates/login.php
+++ templates/login.php
@@ -24,40 +24,6 @@
         </form>
     </body>
 
-<script>
-
-(function(){
-    function validateLoginData(event){
-        var all_data_valid = true;
-
-        var uname = document.querySelector('input[name=user_name]').value;
-        var pass = document.querySelector('input[name=password]').value;
-
-        var error_list = document.querySelector('#ErrorList');
-
-        if(uname == ''){
-            all_data_valid = false;
-            alert('Username is required');
-            console.log('Username is required');
-        }
-        if(pass == ''){
-            all_data_valid = false;
-            alert('Password is required');
-            console.log('Password is required');
-        }
-        
-
-        if(!all_data_valid){
-            event.preventDefault();
-        }
-        
-    }
-
-    var login_btn = document.querySelector('#LoginForm button');
-    login_btn.addEventListener('click',validateLoginData);
-
-})();
-
-</script>
+<script type="text/javascript" src="/filehost/static/script/login_validation.js"></script>
 
 </html>
--- templates/register.php
+++ templates/register.php
@@ -53,63 +53,7 @@
         </form>
 
 
-        <script>
-
-(function(){
-    function addErrorMsg(msg){
-        var error_msg = document.querySelector('#error_msg');
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(msg));
-        error_msg.appendChild(li);
-    }
-
-    function clearErrorMsgs(){
-        var error_msg = document.querySelector('#error_msg');
-        error_msg.innerHTML = '';
-    }
-
-    function validateRegisterData(event){
-        clearErrorMsgs();
-        var all_data_valid = true;
-
-        var uname = document.querySelector('input[name=user_name]').value;
-        var email = document.querySelector('input[name=email]').value;
-        var pass = document.querySelector('input[name=password]').value;
-        var confirm_pass = document.querySelector('input[name=confirm_password]').value;
-
-        if(uname == ''){
-            all_data_valid = false;
-            addErrorMsg('Username is required ');
-        }
-        if(email == ''){
-            all_data_valid = false;
-            addErrorMsg('Email is required');
-        }
-        if(pass == ''){
-            all_data_valid = false;
-            addErrorMsg('Password is required ');
-        }
-        if(confirm_pass == ''){
-            all_data_valid = false;
-            addErrorMsg('Confirm Password is required ');
-        }
-        if(pass != confirm_pass){
-            all_data_valid = false;
-            addErrorMsg('Passwords do not match');
-        }
-
-        if(!all_data_valid){
-            event.preventDefault();
-        }
-        
-    }
-
-    var register_btn = document.querySelector('#RegisterForm button');
-    register_btn.addEventListener('click',validateRegisterData);
-
-})();
-
-</script>
+    <script type="text/javascript" src="/filehost/static/script/register_validation.js"></script>
 
     </body>
 </html>
